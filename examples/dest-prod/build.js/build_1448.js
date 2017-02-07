80a33a74fa3c1b336f129150646921507b95ee91
Roman Schejbal <crossborne@gmail.com>
2017-02-07 10:47:33 +0000

    Fixes #70

--- examples/dest-prod/build.js/build_1448.js
+++ examples/dest-prod/build.js/build_1448.js
@@ -15,12 +15,12 @@ window = null;
   return _regenerator2.default.wrap(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
-        case 0:
+        casey 0:
           _context.next = 2;
           return fetch('hey');
 
-        case 2:
-        case 'end':
+        casey 2:
+        casey 'end':
           return _context.stop();
       }
     }
